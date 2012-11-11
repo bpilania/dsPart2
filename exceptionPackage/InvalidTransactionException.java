83e55b04fec7e9c79a19ab08b3645b378a91e31c
sameer.jagdale <sameerjagdale90@yahoo.co.in>
2012-11-11 07:29:26 -0500

    exception files added

--- exceptionPackage/InvalidTransactionException.java
+++ exceptionPackage/InvalidTransactionException.java
@@ -1,8 +1,20 @@
 package exceptionPackage;
 public class InvalidTransactionException extends Exception{
 
-	public InvalidTransactionException(String msg){
+	int id;
+	String msg;
+	public InvalidTransactionException(int id,String msg){
 		super(msg);
+		this.id=id;
+		this.msg=new String(msg);
 	}
+	
+	public int getId(){
+		return id;
+	}
+	public String getMsg(){
+		return msg; 
+	}
+	
 }
 
